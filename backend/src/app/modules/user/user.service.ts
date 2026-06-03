6791b802dd1a2796b60de6144302204e9751dce1
adnanmahmud0 <adnan99mahmud@gmail.com>
2026-06-04 03:43:41 +0600

    Disable email verification on user registration

--- backend/src/app/modules/user/user.service.ts
+++ backend/src/app/modules/user/user.service.ts
@@ -2,44 +2,19 @@ import { StatusCodes } from 'http-status-codes';
 import { JwtPayload } from 'jsonwebtoken';
 import { USER_ROLES } from '../../../enums/user';
 import ApiError from '../../../errors/ApiError';
-import { emailHelper } from '../../../helpers/emailHelper';
-import { emailTemplate } from '../../../shared/emailTemplate';
 import unlinkFile from '../../../shared/unlinkFile';
-import generateOTP from '../../../util/generateOTP';
 import { IUser } from './user.interface';
 import { User } from './user.model';
-import { debug } from '../../../shared/debug';
 
 const createUserToDB = async (payload: Partial<IUser>): Promise<IUser> => {
-  //set role
+  //set role and verified status
   payload.role = USER_ROLES.USER;
+  payload.verified = true;
+  
   const createUser = await User.create(payload);
   if (!createUser) {
     throw new ApiError(StatusCodes.BAD_REQUEST, 'Failed to create user');
   }
-
-  //send email
-  const otp = generateOTP();
-  debug('user.register.otp_created', { email: createUser.email, otp });
-  const values = {
-    name: createUser.name,
-    otp: otp,
-    email: createUser.email!,
-  };
-  const createAccountTemplate = emailTemplate.createAccount(values);
-  emailHelper.sendEmail(createAccountTemplate);
-  debug('user.register.email_send_attempt', { email: createUser.email });
-
-  //save to DB
-  const authentication = {
-    oneTimeCode: otp,
-    expireAt: new Date(Date.now() + 3 * 60000),
-  };
-  await User.findOneAndUpdate(
-    { _id: createUser._id },
-    { $set: { authentication } }
-  );
-  debug('user.register.otp_saved', { email: createUser.email });
 
   return createUser;
 };
